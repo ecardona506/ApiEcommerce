4caa718e3e800ffd11e01d4cc7e04ae5505b9938
ecardona506 <ecardona506@javerianacali.edu.co>
2026-06-29 22:16:28 -0500

    add CategoryRepository

---
+++ ApiEcommerce/Repository/CategoryRepository.cs
@@ -0,0 +1,58 @@
+using ApiEcommerce.Repository.IRepository;
+
+namespace ApiEcommerce.Repository;
+public class CategoryRepository : ICategoryRepository
+{
+    private readonly ApplicationDbContext _db;
+
+    public CategoryRepository(ApplicationDbContext db)
+    {
+        _db = db;
+    }
+
+    public bool CategoryExists(int id)
+    {
+        return _db.Category.Any(c => c.Id == id);
+    }
+
+    public bool CategoryExists(string name)
+    {
+        return _db.Category.Any(c => c.Name.ToLower().Trim() == name.ToLower().Trim());
+    }
+
+    public bool CreateCategory(Category category)
+    {
+        category.CreatedAt = DateTime.Now;
+        _db.Category.Add(category);
+        return Save();
+    }
+
+    public bool UpdateCategory(Category category)
+    {
+        category.CreatedAt = DateTime.Now;
+        _db.Category.Update(category);
+        return Save();
+    }
+
+    public bool DeleteCategory(Category category)
+    {
+        _db.Category.Remove(category);
+        return Save();
+    }
+
+    public ICollection<Category> GetCategories()
+    {
+        return _db.Category.ToList();
+    }
+
+    public Category GetCategory(int id)
+    {
+        return _db.Category.FirstOrDefault(c => c.Id == id) ?? 
+        throw new InvalidOperationException($"Category with id {id} doesn't exists");
+    }
+
+    public bool Save()
+    {
+        return _db.SaveChanges() >= 0;
+    }
+}
