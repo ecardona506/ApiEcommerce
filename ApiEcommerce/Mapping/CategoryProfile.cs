56e94e1dcf2bd7ff0612d506f64b9362db3f841a
ecardona506 <ecardona506@javerianacali.edu.co>
2026-07-01 12:36:15 -0500

    add category profile

---
+++ ApiEcommerce/Mapping/CategoryProfile.cs
@@ -0,0 +1,13 @@
+using AutoMapper;
+using ApiEcommerce.Models.Dtos;
+
+namespace ApiEcommerce.Mapping;
+
+public class CategoryProfile : Profile
+{
+    public CategoryProfile()
+    {
+        CreateMap<Category, CategoryDto>().ReverseMap();
+        CreateMap<Category, CreateCategoryDto>().ReverseMap();
+    }
+}
