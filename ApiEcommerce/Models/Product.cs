1320c7ff32efedd896a75dc8fc38d8e07f30d500
ecardona506 <ecardona506@javerianacali.edu.co>
2026-07-24 08:48:24 -0500

    feat: add product model

---
+++ ApiEcommerce/Models/Product.cs
@@ -0,0 +1,26 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ApiEcommerce.Models;
+
+public class Product
+{
+    [Key]
+    public int ProductId { get; set; }
+    [Required]
+    public string Name { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    [Range(0, double.MaxValue)]
+    [Column(TypeName = "decimal(18,2)")]
+    public decimal Price { get; set;}
+    public string ImageUrl { get; set; } = string.Empty;
+    public string SKU { get; set; } = string.Empty;
+    [Range(0,int.MaxValue)]
+    public int Stock {get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.Now;
+    public DateTime? UpdateAt {get; set;} = null;
+    public int CategoryId {get; set;}
+    [ForeignKey("CategoryId")]
+    public required Category Category { get; set;}
+}
