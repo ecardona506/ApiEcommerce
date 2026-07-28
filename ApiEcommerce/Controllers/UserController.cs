3de5030f6639b509a1021d7daab58b51a5aa7116
ecardona506 <ecardona506@javerianacali.edu.co>
2026-07-27 23:03:42 -0500

    feat: add get user endpoint

--- ApiEcommerce/Controllers/UserController.cs
+++ ApiEcommerce/Controllers/UserController.cs
@@ -26,5 +26,19 @@ namespace ApiEcommerce.Controllers
             var usersDto = _mapper.Map<List<UserDto>>(users);
             return Ok(usersDto);
         }
+
+        [HttpGet("{id:int}", Name = "GetUser")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public IActionResult GetUser(int id)
+        {
+            var user = _userRepository.GetUser(id);
+            if (user == null)
+            {
+                return NotFound($"User with {id} id doesn't exists");
+            }
+            var userDto = _mapper.Map<UserDto>(user);
+            return Ok(userDto);
+        }
     }
 }
