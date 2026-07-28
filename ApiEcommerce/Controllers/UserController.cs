9557def3cd95bbe3c1ebaf28df18d9e9e22f14ad
ecardona506 <ecardona506@javerianacali.edu.co>
2026-07-27 23:21:50 -0500

    feat: add register user endpoint

--- ApiEcommerce/Controllers/UserController.cs
+++ ApiEcommerce/Controllers/UserController.cs
@@ -1,4 +1,5 @@
-using ApiEcommerce.Models.Dtos; 
+using ApiEcommerce.Models.Dtos;
+using ApiEcommerce.Repository;
 using ApiEcommerce.Repository.IRepository;
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
@@ -40,5 +41,37 @@ namespace ApiEcommerce.Controllers
             var userDto = _mapper.Map<UserDto>(user);
             return Ok(userDto);
         }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> RegisterUser([FromBody] CreateUserDto createUserDto)
+        {
+            if (createUserDto == null || !ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            if (string.IsNullOrWhiteSpace(createUserDto.UserName))
+            {
+                return BadRequest($"The username field is required");                
+            }
+            if (string.IsNullOrWhiteSpace(createUserDto.Password))
+            {
+                return BadRequest($"The password field is required");                
+            }
+            if (!_userRepository.IsUserUnique(createUserDto.UserName))
+            {
+                return BadRequest($"The user already exists");                
+            }
+            var result = await _userRepository.Register(createUserDto);
+            if (result == null)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Error while registering the user");
+            }
+            return CreatedAtRoute("GetUser", new {id=result.Id}, result);
+        } 
     }
 }
