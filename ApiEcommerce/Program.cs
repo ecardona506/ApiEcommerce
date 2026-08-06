a3d6e90d893bc7d5948a81b01e0a2724d993157b
ecardona506 <ecardona506@javerianacali.edu.co>
2026-08-06 14:56:38 -0500

    feat(versioning): add custom dropdown api version in swagger

--- ApiEcommerce/Program.cs
+++ ApiEcommerce/Program.cs
@@ -125,7 +125,10 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json","Api Ecommerce v1");
+    });
 }
 
 app.UseHttpsRedirection();
