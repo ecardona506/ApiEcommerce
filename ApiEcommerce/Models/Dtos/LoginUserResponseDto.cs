93973166367c0252bd61de9057997a4e8d2ff8f2
ecardona506 <ecardona506@javerianacali.edu.co>
2026-08-09 16:40:06 -0500

    feat(identity): add user data dto

--- ApiEcommerce/Models/Dtos/LoginUserResponseDto.cs
+++ ApiEcommerce/Models/Dtos/LoginUserResponseDto.cs
@@ -2,7 +2,7 @@ namespace ApiEcommerce.Models.Dtos;
 
 public class LoginUserResponseDto
 {
-    public RegisterUserDto? User { get; set; }
+    public UserDataDto? User { get; set; }
     public string? Token { get; set; }
     public string? Message { get; set; }
 }
