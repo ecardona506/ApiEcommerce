37a3cc15e0cbbb27791fe9f63c3c0c62012679ec
ecardona506 <ecardona506@javerianacali.edu.co>
2026-08-09 16:10:43 -0500

    feat(identity): add application user model

--- ApiEcommerce/Data/ApplicationDbContext.cs
+++ ApiEcommerce/Data/ApplicationDbContext.cs
@@ -1,14 +1,21 @@
 using Microsoft.EntityFrameworkCore;
 using ApiEcommerce.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
-public class ApplicationDbContext : DbContext
+public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
 {
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
     {
         
     }
 
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+
     public DbSet<Category> Category { get; set; }
     public DbSet<Product> Products { get; set; }
     public DbSet<User> Users { get; set; }
+    public DbSet<ApplicationUser> ApplicationUsers { get; set; }
 }
