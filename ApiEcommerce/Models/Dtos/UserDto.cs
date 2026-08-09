a910a2dcede3f58e4293937f8560897d5edc1fe3
ecardona506 <ecardona506@javerianacali.edu.co>
2026-08-09 18:25:09 -0500

    feat(identity): remove sensitive information from userDto

--- ApiEcommerce/Models/Dtos/UserDto.cs
+++ ApiEcommerce/Models/Dtos/UserDto.cs
@@ -5,6 +5,4 @@ public class UserDto
     public string? Id { get; set; }
     public string? Name { get; set; }
     public string? UserName { get; set; }
-    public string? Password { get; set; }
-    public string? Role { get; set; }
 }
