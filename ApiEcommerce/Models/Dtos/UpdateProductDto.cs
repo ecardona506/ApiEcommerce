4ae3e7753c183f9fc45182c3e8391a7213386f7e
ecardona506 <ecardona506@javerianacali.edu.co>
2026-08-09 19:52:25 -0500

    feat: update dtos to support image url local

--- ApiEcommerce/Models/Dtos/UpdateProductDto.cs
+++ ApiEcommerce/Models/Dtos/UpdateProductDto.cs
@@ -4,7 +4,8 @@ public class UpdateProductDto
     public string Name { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
     public decimal Price { get; set;}
-    public string ImageUrl { get; set; } = string.Empty;
+    public string? ImageUrl { get; set; }
+    public IFormFile? Image { get; set; }
     public string SKU { get; set; } = string.Empty;
     public int Stock {get; set; }
     public DateTime? UpdateAt {get; set;} = null;
